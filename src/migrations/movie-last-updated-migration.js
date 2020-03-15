a899266d9dccab38a3fe287a27cd68a9bdd4bd53
Brian Andere <briancollins081@gmail.com>
2020-03-15 18:29:13 +0300

    Finished chapter 3, began resiliency

--- src/migrations/movie-last-updated-migration.js
+++ src/migrations/movie-last-updated-migration.js
@@ -28,13 +28,13 @@ require("dotenv").config()
     // check that its type is a string
     // a projection is not required, but may help reduce the amount of data sent
     // over the wire!
-    const predicate = { somefield: { $someOperator: true } }
-    const projection = {}
+    const predicate = { lastupdated: { $exists: true }, lastupdated:{$type:"string"} }
+    const projection = {lastupdated:1}
     const cursor = await mflix
       .collection("movies")
       .find(predicate, projection)
       .toArray()
-    const moviesToMigrate = cursor.map(({ _id, lastupdated }) => ({
+    const moviesToMigrate = cursor.map(({ _id, lastupdated }) => ({      
       updateOne: {
         filter: { _id: ObjectId(_id) },
         update: {
@@ -47,7 +47,9 @@ require("dotenv").config()
       `Found ${moviesToMigrate.length} documents to update`,
     )
     // TODO: Complete the BulkWrite statement below
-    const { modifiedCount } = await "some bulk operation"
+    const { modifiedCount } = await mflix.collection("movies").bulkWrite(
+      moviesToMigrate
+    );
 
     console.log("\x1b[32m", `${modifiedCount} documents updated`)
     client.close()
